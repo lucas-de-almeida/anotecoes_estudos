fad80d3f03824005fc3d36e8f330f5dd3048ac98
lucas de almeida <lucasde.almeida@hotmail.com>
2021-10-21 19:00:50 -0300

    commit

--- 3-dart_poo/lib/7_polimorfismo/1_polimorfismo.dart
+++ 3-dart_poo/lib/7_polimorfismo/1_polimorfismo.dart
@@ -5,9 +5,15 @@ import 'package:dart_poo/7_polimorfismo/anestesista.dart';
 import 'package:dart_poo/7_polimorfismo/medico.dart';
 import 'package:dart_poo/7_polimorfismo/obstetra.dart';
 import 'package:dart_poo/7_polimorfismo/pediatra.dart';
+import 'package:dart_poo/7_polimorfismo/residente_anestesia.dart';
 
-void main(List<String> args) {
-  var medicos = <Medico>[Anestesista(), Obstetra(), Pediatra()];
+void main() {
+  var medicos = <Medico>[
+    ResidenteAnestesia(),
+    Anestesista(),
+    Obstetra(),
+    Pediatra()
+  ];
 
   for (var medico in medicos) {
     medico.operar();
---
+++ 3-dart_poo/lib/7_polimorfismo/residente_anestesia.dart
@@ -0,0 +1,8 @@
+import 'package:dart_poo/7_polimorfismo/anestesista.dart';
+
+class ResidenteAnestesia extends Anestesista {
+  @override
+  void operar() {
+    print('esterelizar equipamentos');
+  }
+}
