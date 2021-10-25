c0b308e55670cfa7825012a4a5394ed0ce38d484
lucas de almeida <lucasde.almeida@hotmail.com>
2021-10-25 14:38:18 -0300

    event loop

---
+++ 4-dart_async/lib/5_event_loop/1_event_loop.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names
+
+//vid main é uma kicro task tem prioridade na excecussão
+import 'dart:async';
+
+void main() {
+  print('incio main');
+  scheduleMicrotask(() => print('microtask 1'));
+  Future.delayed(Duration(seconds: 1), () => print('Future #1 delayed'));
+  Future(() => print('Future #2 '));
+  Future(() => print('Future #3 '));
+
+  scheduleMicrotask(() => print('microtask 2'));
+
+  print('fim main');
+}
