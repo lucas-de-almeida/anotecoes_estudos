c988472a0894722009ad098a5c790b5ef2bf0fd2
lucas de almeida <lucasde.almeida@hotmail.com>
2021-10-27 19:12:50 -0300

    periodic

---
+++ 4-dart_async/lib/10_streams/1_periodic.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names
+
+Future<void> main() async {
+  print('inicio');
+  final interval = Duration(seconds: 2);
+  final stream = Stream<int>.periodic(interval, callback);
+
+// await for
+  await for (var i in stream) {
+    print(i);
+  }
+  print('fim');
+}
+
+int callback(int value) {
+  print('o valor é $value');
+
+  return (value + 1) * 2;
+}
---
+++ 4-dart_async/lib/10_streams/z1_streams.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+
+//assincrono no dart são representados por apenas duas classes 
+// future (futuro)
+/**
+ * um future é usado para representar um valor ou um erro que estara disponivel
+ * no futuro voce recebera um unico resultado ou um erro
+ * then traz um resultado e um erro no try cath
+ * 
+ */
+//stream (fluxo)
+/**
+ * uma stream tem a mesma caracteristica de um future 
+ * ela é uma combinaão de futures dessa forma poderemos receber varios valores 
+ * ate ela ser fechada.
+ * ela é um canal aberto so apra essa comunicação quando eu fecha-la
+ */
